2713b736755aa26942e573cda36b8ad42b1c1474
Yahya <yahyahussein670@gmail.com>
2022-07-21 14:00:47 +0300

    Done.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
